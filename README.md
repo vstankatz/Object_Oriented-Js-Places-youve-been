ca51fab925f716f26413bc85640aec9a196d2e7e
Student <example@example.com>
2019-11-04 11:39:27 -0800

    add output

---
+++ README.md
@@ -0,0 +1,6 @@
+## Specs
+
+|Specs|Input|Output|
+|-|-|-|
+|The program will store "place" information |Location: Portland, Fun-Things: Dogs, etc.|Portland, Dogs, Etc. |
+|The program will assign a unique ID to each location|||
--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,8 @@ PlacesBeen.prototype.assignId = function() {
   return this.currentId;
 }
 
-PlacesBeen.prototype.findLocation = function (id) {
+PlacesBeen.prototype.findPlace = function(id) {
+  // console.log(fuck);
   for (var i=0; i< this.places.length; i++) {
     if (this.places[i]) {
       if (this.places[i].id == id) {
@@ -25,7 +26,7 @@ PlacesBeen.prototype.findLocation = function (id) {
 }
 
 
-function place(placeName, landMarks, timeOfYear, funThings, foodHere) {
+function Place(placeName, landMarks, timeOfYear, funThings, foodHere) {
   this.placeName = placeName,
   this.landMarks = landMarks,
   this.timeOfYear = timeOfYear,
@@ -33,8 +34,28 @@ function place(placeName, landMarks, timeOfYear, funThings, foodHere) {
   this.foodHere = foodHere
 }
 
-var placesBeen = new PlacesBeen();
-var place1 = new place("Portland", "Waterfront", "Spring", "Night Market", "Food Trucks");
-var place2 = new place("Long Beach", "The Pike", "Summer", "LA street market", "Tacos");
-placesBeen.addPlace(place1);
-placesBeen.addPlace(place2);
+var PlacesBeen = new PlacesBeen();
+var place1 = new Place("Portland", "Waterfront", "Spring", "Night Market", "Food Trucks");
+var place2 = new Place("Long Beach", "The Pike", "Summer", "LA street market", "Tacos");
+PlacesBeen.addPlace(place1);
+PlacesBeen.addPlace(place2);
+
+var results1 = PlacesBeen.findPlace(1);
+var results2 = PlacesBeen.findPlace(2);
+
+// var placesBeen = new PlacesBeen();
+//
+// function dislayLocations (placesToDisplay) {
+//
+// }
+
+
+$(document).ready(function() {
+var pickedPlace1 = document.getElementById("place1");
+var pickedPlace2 = document.getElementById("place2");
+pickedPlace1.onclick = function place() {
+  console.log(place1);
+  $(".output").text(place1.placeName + " " + place1.landMarks);
+}
+
+});
